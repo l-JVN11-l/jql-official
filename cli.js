3247957a73bf1902b69bb5ea30a0633e2af56686
Jivin Mazumder <jmazumder@ebnet.org>
2022-06-16 22:18:13 -0400

    1.1.0

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const [,, ...args] = process.argv
+const fs = require('fs')
+const JQL = require('./dist/dlang.min.js')
+
+fs.readFile(args[0], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const app = new JQL(data)
+  app.run()
+});
